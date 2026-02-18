6661ccd6d2eb992eeca35dd9c970e69a43540307
nsanjay01 <sanjayvnr296@gmail.com>
2026-02-18 17:44:00 +0530

    Fix: apply .gitignore and stop tracking build files

--- build/Debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "B-U585I-IOT02A_tinyML_IMU.map"
-  )
-endif()
